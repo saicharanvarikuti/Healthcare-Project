fc990d5e4fed5579d663fe5aa1b15790ab9e32a9
saicharanvarikuti <134496992+saicharanvarikuti@users.noreply.github.com>
2024-01-18 00:00:32 +0530

    react app created
    front end started

---
+++ FrontEnd/healthcare/src/components/navbar.js
@@ -0,0 +1,19 @@
+function Navbar(){
+    return (
+        <div className="Nav-component">
+            <div className="Nav-container p-100 flex justify-between ">
+                <div className="Nav-left-wrapper">
+                    <p>Home page</p>
+                </div>
+                <div className="Nav-middle-wrapper">
+                <p>Logo</p>
+                </div>
+                <div className="Nav-right-wrapper">
+                <p>Sign In/ Sign Up</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
